b319f88ec72a2cbf2c20583c13805a2c6905ec3e
abbasimohsin392-prog <abbasimohsin392@gmail.com>
2026-09-04 23:50:01 +0500

    Use canonical host in robots sitemap reference

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.starlightai.site/sitemap.xml',
+    sitemap: 'https://starlightai.site/sitemap.xml',
   }
 }
