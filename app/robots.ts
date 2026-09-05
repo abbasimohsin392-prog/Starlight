193933622303f7dff1416d72f2f30440dfe004c9
abbasimohsin392-prog <abbasimohsin392@gmail.com>
2026-09-05 14:05:44 +0500

    Allow approved AI crawlers for search visibility

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      { userAgent: '*', allow: '/' },
+      {
+        userAgent: ['GPTBot', 'ClaudeBot', 'Google-Extended', 'Applebot-Extended', 'Amazonbot', 'Bytespider', 'CCBot', 'meta-externalagent'],
+        allow: '/',
+      },
+    ],
     sitemap: 'https://starlightai.site/sitemap.xml',
   }
 }
