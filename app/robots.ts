1f0725997de692a0e2aa86e11936503edd47228a
abbasimohsin392-prog <abbasimohsin392@gmail.com>
2026-09-05 14:17:20 +0500

    SEO: keep robots policy clean and sitemap discoverable

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,10 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      { userAgent: '*', allow: '/' },
-      {
-        userAgent: ['GPTBot', 'ClaudeBot', 'Google-Extended', 'Applebot-Extended', 'Amazonbot', 'Bytespider', 'CCBot', 'meta-externalagent'],
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: 'https://starlightai.site/sitemap.xml',
   }
 }
